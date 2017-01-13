808f973fe4b7305fe9de92ee9702faa3c172ae39
nicoandresr <nicoandres.rodriguez@gimail.com>
2017-01-13 10:49:11 -0500

    Update for send config string in Rest /config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
-var express = require("express");
+var express = require('express');
 var ServeDist = (function () {
-    function ServeDist(_port) {
+    function ServeDist(_port, _config) {
         this._port = _port;
+        this._config = _config;
         this._msgConsoleStartApp = "App listening on port: ";
         this._app = express();
         this.setupExpressServer();
@@ -10,6 +11,9 @@ var ServeDist = (function () {
     ServeDist.prototype.setupExpressServer = function () {
         var _this = this;
         this._app.use("/", express.static("dist"));
+        if (this._config) {
+            this._app.get("/config", function (req, res) { res.send(_this._config); });
+        }
         this._app.listen(this._port, function () { return console.log(_this.getInitialMsg()); });
     };
     ServeDist.prototype.getInitialMsg = function () {
--- index.ts
+++ index.ts
@@ -4,13 +4,18 @@ export class ServeDist {
   private _app: express.Application;
   private _msgConsoleStartApp: string = "App listening on port: ";
 
-  constructor(private _port: number) {
+  constructor(private _port: number, private _config: string) {
     this._app = express();
     this.setupExpressServer();
   }   
 
   private setupExpressServer(): void {
     this._app.use("/", express.static("dist"));
+
+    if (this._config) {
+      this._app.get("/config", (req, res) => { res.send(this._config); });
+    }
+
     this._app.listen(this._port, () => console.log(this.getInitialMsg()));
   }   
 
